15dd3205c6eb3fa4e060eca2ea070377f3279883
Nazmul Islam <litocse@gmail.com>
2024-06-06 03:44:22 +0600

    Add project files.

---
+++ LeetCode/CheckPalindrome.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode
+{
+    public class CheckPalindrome
+    {
+        public static bool IsPalindrome(int number)
+        {
+            // 161
+            // 161/10 = 16...1
+            // 16/10=1...6
+            // 1/10=
+
+            int quotient = 121;
+            if (quotient < 0) return false;
+
+            int original = quotient;
+            int reversed = 0;
+            while (quotient != 0)
+            {
+                reversed = reversed * 10 + quotient % 10; // reversed = 0*10 + 121%10=0+1=1 > 1*10+12%10=10+2=12 > 12*10 + 1%10=120+1=121
+                //x /= 10;
+                quotient = quotient / 10; //x= 121/10 =12  > X= 12/10 =1 > 1 / 10=0
+            }
+
+            return original == reversed;
+
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    if (number[i] != number[number.Length - 1 - i])
+            //    {
+            //        return false;
+            //    }
+            //}
+            //return true;
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        #region Check Palindrome Compare with reverse
+        //Console.WriteLine("Enter Number:");
+        //string number = Console.ReadLine();
+        //Console.WriteLine(CheckPalindrome.IsPalindrome(number));
+        #endregion
+
+        #region Check Palindrome Compare with reverse(n(Log n))
+        Console.WriteLine("Enter Number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(CheckPalindrome.IsPalindrome(number));
+        #endregion
+    }
+}
