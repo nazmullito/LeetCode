54fa67672915fbb85828ddfe90d71b69d3f99a82
Nazmul Islam <litocse@gmail.com>
2024-06-08 01:32:59 +0600

    Check Palindrome Without Converting to String

---
+++ LeetCode/CheckPalindromeWithoutConvertingtoString.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    public class CheckPalindromeWithoutConvertingtoString
+    {
+        public static bool IsPalindrome(int number)
+        {
+            int original = number;
+            int reverse = 0;
+
+            if (number < 0)
+                return false;
+
+            while (number != 0)
+            {
+                reverse = reverse * 10 + number % 10;
+
+                number = number / 10;
+            }
+            if (original == reverse)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,16 +4,16 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        #region Check Palindrome Compare with reverse
+        #region Check Palindrome O(n)
         //Console.WriteLine("Enter Number:");
         //string number = Console.ReadLine();
         //Console.WriteLine(CheckPalindrome.IsPalindrome(number));
         #endregion
 
-        #region Check Palindrome Compare with reverse(n(Log n))
+        #region Check Palindrome Without Converting to String O(n)
         Console.WriteLine("Enter Number:");
         int number = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(CheckPalindrome.IsPalindrome(number));
+        Console.WriteLine(CheckPalindromeWithoutConvertingtoString.IsPalindrome(number));
         #endregion
     }
 }
