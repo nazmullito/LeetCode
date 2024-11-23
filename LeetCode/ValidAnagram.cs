920fc6b9d8d5464e9aed8019c5e28d08104f60e0
Nazmul Islam <nazmul.islam@adndiginet.com>
2024-11-24 03:25:49 +0600

    242. Valid Anagram

---
+++ LeetCode/ValidAnagram.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class ValidAnagram
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+            else
+            {
+                int[] count = new int[26];
+                for (int i = 0; i < s.Length; i++)
+                {
+                    count[s[i] - 'a']++;
+                }
+                for (int i = 0; i < t.Length; i++)
+                {
+                    count[t[i] - 'a']--;
+                }
+                for (int i = 0; i < count.Length; i++)
+                {
+                    if (count[i] != 0)
+                        return false;
+                }
+                return true;
+            }
+        }
+    }
+}
